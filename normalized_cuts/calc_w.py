f1a4f3ae8aef49cb838c7c61958008414f558b02
stillbreeze <asharjaved7@gmail.com>
2016-03-14 00:17:55 +0530

    N-Cut similarity matrix-spatial

--- normalized_cuts/calc_w.py
+++ normalized_cuts/calc_w.py
@@ -6,41 +6,38 @@ from scipy.sparse import csr_matrix
 import h5py
 
 
-def calcNorm(i,j,img,W_i,sigmaI,r):
+def calcNorm(i,j,img,weight_matrix,sigmaI,sigmaS,r):
 	row=img.shape[0]
 	col=img.shape[1]
 	for x in xrange(row):
 		for y in xrange(col):
 			dist=sqrt(pow((i-x),2)+pow((j-y),2))
 			if dist<r:
-				temp=exp((-pow(cv2.norm(img[i][j],img[x][y],cv2.NORM_L1),2))/sigmaI)
-				W_i[i+j][x+y]=temp
+				temp_s=exp((-pow(cv2.norm((i,j),(x,y),cv2.NORM_L2),2)))/pow(sigmaS,2)
+				temp_i=exp((-pow(cv2.norm(img[i][j],img[x][y],cv2.NORM_L2),2)))/pow(sigmaI,2)
+				weight_matrix[i+j][x+y]=temp_s*temp_i
 
-def spatialAffinity(img,r):
-	pass
-
-def intensityAffinity(img,r):
+def affinityMatrix(img,r):
 	row=img.shape[0]
 	col=img.shape[1]
 	N=row*col
-	print N
-	W_i=np.zeros((N,N))
-	print W_i.shape
-	(means, stds) = cv2.meanStdDev(img)	
-	sigmaI=2*(pow(stds[0][0],2)+pow(stds[1][0],2)+pow(stds[2][0],2))
+	weight_matrix=np.zeros((N,N))
+	weight_matrix=weight_matrix.astype("float16",copy=False)
+	# (means, stds)=cv2.meanStdDev(img)	
+	# sigmaI=2*(pow(stds[0][0],2)+pow(stds[1][0],2)+pow(stds[2][0],2))
+	sigmaI=500
+	sigmaS=4
 	for i in xrange(row):
 		for j in xrange(col):
-			calcNorm(i,j,img,W_i,sigmaI,r)
+			calcNorm(i,j,img,weight_matrix,sigmaI,sigmaS,r)
 			print i,j
-	with h5py.File('W_i.h5', 'w') as hf:
-	    hf.create_dataset('W_i', data=W_i)
+	with h5py.File('weight_matrix.h5', 'w') as hf:
+		hf.create_dataset('weight_matrix', data=weight_matrix)
 
 if __name__ == "__main__":
 	img=cv2.imread('./colors.jpg',3)
 	img=cv2.resize(img,(150,150))
-	cv2.imshow('',img)
 	r=2.5
-	intensityAffinity(img,r)
-	spatialAffinity(img,r)
+	affinityMatrix(img,r)
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
