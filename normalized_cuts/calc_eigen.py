0b33cda5acc4582c5a5b718f8c56b59504b05308
stillbreeze <asharjaved7@gmail.com>
2016-04-05 03:31:12 +0530

    Ncut recursive call + refactor

--- normalized_cuts/calc_eigen.py
+++ normalized_cuts/calc_eigen.py
@@ -5,9 +5,7 @@ from math import sqrt, pow, exp
 from scipy.sparse import linalg
 import h5py
 
-if __name__ == "__main__":
-	with h5py.File('weight_matrix.h5', 'r') as hf:
-		W=np.array(hf.get('weight_matrix'))
+def eigen(W):
 	d=np.sum(W,1)
 	D=np.diag(d)
 
@@ -19,19 +17,5 @@ if __name__ == "__main__":
 	del d
 	del W
 
-	print np.isfinite(DW).all()
-	print np.isfinite(D).all()
-
-	print (D.transpose() == D).all()
-	print (DW.transpose() == DW).all()
-
-	print np.linalg.det(DW)
-	print np.linalg.det(D)
-
 	eigVal,eigVec=linalg.eigs(A=DW,k=2,M=D,which='SM')
-	print '\n'
-	print eigVal
-	print eigVec
-	with h5py.File('eig.h5', 'w') as hf:
-		hf.create_dataset('eigVal', data=eigVal)
-		hf.create_dataset('eigVec', data=eigVec)
+	return eigVec
--- normalized_cuts/nCut.py
+++ normalized_cuts/nCut.py
@@ -1,17 +1,15 @@
 import h5py
 import numpy as np
 import calc_ncut as ncut
+import calc_eigen as eig
 import scipy.optimize
+import cv2
 
 
-def NcutRecursive():
-	with h5py.File('eig.h5', 'r') as hf:
-		eigVal=np.array(hf.get('eigVal'))
-		eigVec=np.array(hf.get('eigVec'))
 
-	with h5py.File('weight_matrix.h5', 'r') as hf:
-		W=np.array(hf.get('weight_matrix'))
+def NcutRecursive(W,segFinal,imgSeg,threshcut,thresharea):
 
+	eigVec=eig.eigen(W)
 	d=np.sum(W,1)
 	D=np.diag(d)
 
@@ -27,5 +25,26 @@ def NcutRecursive():
 	A=np.where(eigVecSmall>thresh)
 	B=np.where(eigVecSmall<=thresh)
 
+	print A
+	print '\n'
+	print B
+
+	if (ncut.ncutValue(thresh,eigVecSmall,D,W,d) > threshcut) or (A.shape[0]<thresharea) or (B.shape[0]<thresharea):
+		return 1
+	segFinal.append((A,B))
+	res=NcutRecursive(np.take(W[A],A,1),segFinal,np.take(imgSeg,A),threshcut,thresharea)
+	res=NcutRecursive(np.take(W[B],A,1),segFinal,np.take(imgSeg,B),threshcut,thresharea)
+	print 'Writing segments'
+	with h5py.File('segFinal.h5', 'w') as hf:
+		hf.create_dataset('segFinal', data=segFinal)
+
 if __name__ == "__main__":
-	NcutRecursive()
+	img=cv2.imread('./colors.jpg',0)
+	imgSeg=cv2.resize(img,(50,50))
+
+	with h5py.File('weight_matrix.h5', 'r') as hf:
+		W=np.array(hf.get('weight_matrix'))
+	segFinal=[]
+	threshcut=0.2
+	thresharea=13
+	NcutRecursive(W,segFinal,imgSeg,threshcut,thresharea)
